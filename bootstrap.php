eaa60893ae7ec5c9529be8c9c7b423dc170839ca
Roberts Sukonovs <roberts.sukonovs@gmail.com>
2020-04-06 17:08:29 +0300

    Add simple bootstrap code.

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = ["/src/Entities"];
+$isDevMode = true;
+
+$dbParams = [
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => 'root',
+    'dbname'   => 'app',
+];
+
+$builder = new DI\ContainerBuilder();
+$container = $builder->build();
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
